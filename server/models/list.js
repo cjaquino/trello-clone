966d966ebd938700442e0d8c352668111c8574c7
Vincent Bundage <vincentbundage@gmail.com>
2021-06-15 11:28:27 -0600

    fix: updated id types to String

--- server/models/list.js
+++ server/models/list.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ListSchema = new Schema({
+const ListSchema = new Schema(
+  {
     title: {
       type: String,
-      required: [true, "The List title is required"]
+      required: [true, "The List title is required"],
     },
     boardId: {
-      type: Number,
-      required: [true, "The List boardId is required"]
+      type: String,
+      required: [true, "The List boardId is required"],
     },
     position: {
       type: Number,
-      required: [true, "The List position is required"]
+      required: [true, "The List position is required"],
     },
-    cards: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Card"
-    }]
+    cards: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Card",
+      },
+    ],
   },
   { timestamps: true }
-)
+);
 
-const List = mongoose.model('List', ListSchema);
+const List = mongoose.model("List", ListSchema);
 
 module.exports = List;
