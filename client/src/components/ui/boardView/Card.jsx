bd4b60edd7ce45a0bfab841133fcb13fe3c067d4
Vincent Bundage <vincentbundage@gmail.com>
2021-06-17 10:26:26 -0600

    update: card due date

--- client/src/components/ui/boardView/Card.jsx
+++ client/src/components/ui/boardView/Card.jsx
@@ -14,7 +14,7 @@ const Card = ({ card }) => {
         </div>
         <div className="card-icons">
           <i className="clock-icon sm-icon overdue-recent completed">
-            Aug 4
+            {(new Date(card.dueDate)).toDateString().slice(4, 11)}
           </i>
           <i className="description-icon sm-icon"></i>
           <i className="comment-icon sm-icon"></i>
