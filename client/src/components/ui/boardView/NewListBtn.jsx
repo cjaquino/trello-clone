99aefeb71785fba3efa48969bdbce55e471d3812
grahamhub <grammar@hey.com>
2021-06-18 11:26:54 -0500

    feature: toggle add list form

--- client/src/components/ui/boardView/NewListBtn.jsx
+++ client/src/components/ui/boardView/NewListBtn.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewListBtn = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleForm = (ev) => {
+    ev.preventDefault();
+    setShowForm(!showForm);
+  };
+
+  let selected = showForm ? "selected" : "";
+
   return (
-    <div id="new-list" className="new-list">
-      <span>Add a list...</span>
+    <div id="new-list" className={`new-list ${selected}`}>
+      <span onClick={toggleForm}>Add a list...</span>
       <input type="text" placeholder="Add a list..." />
       <div>
         <input type="submit" className="button" value="Save" />
-        <i className="x-icon icon"></i>
+        <i onClick={toggleForm} className="x-icon icon"></i>
       </div>
     </div>
   );
